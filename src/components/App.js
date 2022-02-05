72926022906612385966f29b1cea776c8ac474a6
pratiked7 <pratiked7@gmail.com>
2022-02-05 20:35:37 +0530

    ethereum account

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,43 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 //import logo from '../logo.png';
 import './App.css';
 import {create} from "ipfs-http-client";
+import Web3 from "web3";
 
 const ipfs = create("https://ipfs.infura.io:5001/api/v0");
 
 
 const App = () => {
 
+  useEffect(() => {
+    loadWeb3();
+  }, []);
+
   const [imageBuffer, setImageBuffer] = useState(null);
   const [imageUrl, setImage] = useState("https://ipfs.infura.io/ipfs/QmRtumqmm52d6rTEjyjupuuz2xVadUyT9ocRhWWB7TS3xB");
+  const [ethAccount, setEthAccount] = useState();
+
+  const loadBlockchainData = async () => {
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+    setEthAccount(accounts[0]);
+  }
+
+  const loadWeb3 = async () => {
+    
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      window.ethereum.enable();
+      //await window.ethereum.sendAsync("eth_requestAccounts");
+      await loadBlockchainData();
+    } else if (window.web3) {
+      //window.web3 = new Web3(window.web3.currentProvider);
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert("Please use metamask!")
+    }
+  }
 
   const onSubmit = async (event) => {
     event.preventDefault();
@@ -43,26 +71,15 @@ const App = () => {
   return (
     <div>
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a
-          className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <div className="navbar-brand col-sm-3 col-md-2 mr-0">
           Meme
-        </a>
+        </div>
       </nav>
       <div className="container-fluid mt-5">
         <div className="row">
           <main role="main" className="col-lg-12 d-flex text-center">
             <div className="content mr-auto ml-auto">
-              <a
-                href="http://www.dappuniversity.com/bootcamp"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={imageUrl} className="App-logo" alt="logo" />
-              </a>
+            <img src={imageUrl} className="App-logo" alt="logo" />
               <p>&nbsp;</p>
               <h2>Change Image</h2>
               <form onSubmit={onSubmit}>
